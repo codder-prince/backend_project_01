60a4e7291754ffbd72d8e0b7a8f7122c368ec9ca
codder-prince <princechoudhary26102610@gmail.com>
2026-03-14 00:03:22 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,65 @@
+// require('dotenv').config({path: './env'})
+//            or
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants"
+
+dotenv.config({
+    path : './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1st Approach to connect DB :
+
+// Using => IIFE = Immediately Invoked Function Expression in JS
+//Matlab function jo define hote 
+// hi turant execute ho jata hai.
+
+
+/*
+import express from "express";
+const app = express()
+;( async () =>{
+    try {
+        await mongoose.connect(`${process.env.
+        MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRROR", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port $
+                {process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err
+    }
+})()
+
+*/
